07be53bea10556695b45fa4f7c99dee6a4906f3f
Ed Z <hzouah@connect.ust.hk>
2023-03-01 16:27:32 +0800

    remap <leader>T to toggle floaterm

--- after/plugin/floatterm.lua
+++ after/plugin/floatterm.lua
@@ -1,3 +1,3 @@
-vim.keymap.set("n", "<leader>t", ":FloatermToggle<CR>")
-vim.api.nvim_set_keymap('t', '<leader>t', '<C-\\><C-n>:FloatermToggle<CR>', {noremap = true, silent = true})
+vim.keymap.set("n", "<leader>T", ":FloatermToggle<CR>")
+vim.api.nvim_set_keymap('t', '<leader>T', '<C-\\><C-n>:FloatermToggle<CR>', {noremap = true, silent = true})
 
