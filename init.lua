68cee9397ef1518ffb3c194ec1720b8c514c014c
ZOU Hetai <33616271+JXNCTED@users.noreply.github.com>
2024-09-16 11:11:51 +0800

    add toggle keybinding

--- init.lua
+++ init.lua
@@ -6,5 +6,5 @@ if vim.g.neovide then
     vim.g.neovide_floating_blur_amount_y = 2.0
     vim.g.neovide_cursor_vfx_mode = "railgun"
     vim.g.neovide_refresh_rate = 93
-    vim.g.neovide_fullscreen = true
+    vim.g.neovide_fullscreen = false
 end
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- toggle neovide full screen
+vim.keymap.set(
+    "n",
+    "<leader>fs",
+    ":let g:neovide_fullscreen = !g:neovide_fullscreen<CR>",
+    { noremap = true, silent = true }
+)
