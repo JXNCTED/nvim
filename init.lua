b1278604f3a4664ababd6ddc62f2a09035cf800f
EdZ <zouhetai@outlook.com>
2025-01-19 18:32:10 +0800

    update plugins

--- init.lua
+++ init.lua
@@ -5,7 +5,7 @@ if vim.g.neovide then
     vim.g.neovide_floating_blur_amount_x = 2.0
     vim.g.neovide_floating_blur_amount_y = 2.0
     vim.g.neovide_cursor_vfx_mode = "railgun"
-    vim.g.neovide_refresh_rate = 144
+    vim.g.neovide_refresh_rate = 120
     vim.g.neovide_fullscreen = false
     vim.o.guifont = "DejaVuSansM Nerd Font:h10"
 end
